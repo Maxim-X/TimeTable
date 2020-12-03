a1b08809dcdeed29e3e4ec768a976f325463adec
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-12-03 18:30:54 +0500

    Update Authorization

--- assets/general/class.authorization.php
+++ assets/general/class.authorization.php
@@ -5,6 +5,16 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/assets/main/RedBean.php");
  */
 class Authorization 
 {
+	public static function email_valid($email){
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    		return ["status" => true, "message" => "E-mail адрес указан верно!"];
+		}else{
+			return ["status" => false, "message" => "E-mail адрес указан неверно!"];
+		}
+	}
+
+	public static function 
+
 	public static function auth(){ //$login, $password
 		echo R::testConnection();
 	}	

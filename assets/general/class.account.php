6b4bd8ca4b41a59126d54d15a6fd4af10c4d3e27
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2020-12-23 07:45:32 +0500

    Доработка класса Account

--- assets/general/class.account.php
+++ assets/general/class.account.php
@@ -13,28 +13,55 @@ class Account
 	public static $MIDDLENAME;
 	protected static $ACCESSLEVEL;
 
+	 
+	# SESSION
+	private static $sess_user_id = "user_id";
+
 	public static function init()
 	{
 		if (self::auth_check()) {
-			self::$AUTH = true;
-			self::$ID = $_SESSION['user_id'];
-			// $user_data = R::
-			self::$EMAIL = 		$user_data["email"];
-			self::$LOGIN = 		$user_data["login"];
-			self::$NAME = 		$user_data["name"];
-			self::$SURNAME = 		$user_data["surname"];
-			self::$MIDDLENAME = 	$user_data["middlename"];
-			self::$ACCESSLEVEL = 	$user_data["accesslevel"];
+			$user_data_db = R::findOne('accounts', $_SESSION[$sess_user_id]);
+			if ($user_data_db) {
+				self::$AUTH 		= true;
+				self::$ID 			= $user_data_db->id;
+				self::$EMAIL 		= $user_data_db->email;
+				// self::$LOGIN 		= $user_data["login"];
+				// self::$NAME 		= $user_data["name"];
+				// self::$SURNAME 		= $user_data["surname"];
+				// self::$MIDDLENAME 	= $user_data["middlename"];
+				// self::$ACCESSLEVEL 	= $user_data["accesslevel"];
+			}
 		}
 	}
-	public static function auth($login, $password){
-		
-		echo "string";
+	public static function auth($email, $password){
+		$user_data_db = R::findOne('accounts', 'email = ?', array($email));
+		if ($user_data_db) {
+			if (password_verify($password.$user_data_db->salt, $user_data_db->password) ) {
+				$_SESSION[$sess_user_id] = $user_data_db->id;
+				return array("status" => true, "user_id" => $user_data_db->id, "message" => "Успешная авторизация!");
+			}else{
+				return array("status" => false, "message" => "Неверно введен пароль!");
+			}
+			
+		}else{
+			return array("status" => false, "message" => "Пользователь с таким E-mail не найден!");
+		}
 		// echo R::testConnection();
 		// $_SESSION['user_id'] = 1;
 	}
 
-	# password ; mail ; salt
+	public static function exit(){
+		unset($_SESSION[$sess_user_id]);
+		if (!isset($_SESSION[$sess_user_id])) {
+			return array("status" => true, "message" => "Вы вышли из аккаунта!");
+		}else{
+			return array("status" => false, "message" => "Ошибка выхода из аккаунта!");
+		}
+	}
+
+	// Регистрация пользователя
+	// => $user_info - массив данных для регистрации
+	// 	  [ "email" - Почта, "password" - Пароль ]
 	public static function signup($user_info){
 		$user_info = self::trim_info($user_info);
 
@@ -61,7 +88,7 @@ class Account
 		$user->email 	= $user_info['email'];
 		$user->password = $password;
 		$user->salt = $salt;
-		// R::store($user);
+		R::store($user);
 
 		return ["status" => true, "message" => "Успешная регистрация! ".$password];
 	}
@@ -73,7 +100,7 @@ class Account
 	}
 
 	public static function auth_check(){
-		if (isset($_SESSION['user_id'])) {
+		if (isset($_SESSION[$sess_user_id])) {
 			return true;
 		}
 		return false;
@@ -81,33 +108,33 @@ class Account
 
 	private static function email_valid($email){
 		if (!isset($email) || empty($email)) {
-			return ["status" => false, "message" => "E-mail адрес не указан!"];
+			return array("status" => false, "message" => "E-mail адрес не указан!");
 		}
 		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    		return ["status" => true, "message" => "E-mail адрес указан верно!"];
+    		return array("status" => true, "message" => "E-mail адрес указан верно!");
 		}else{
-			return ["status" => false, "message" => "E-mail адрес указан неверно!"];
+			return array("status" => false, "message" => "E-mail адрес указан неверно!");
 		}
 	}
 
 	private static function password_valid($password){
 		if (!isset($password) || empty($password)) {
-			return ["status" => false, "message" => "Пароль не указан!"];
+			return array("status" => false, "message" => "Пароль не указан!");
 		}
 		if (strlen($password) <= 8) {
-        	return ["status" => false, "message" => "Пароль должен содержать не менее 8 символов!"];
+        	return array("status" => false, "message" => "Пароль должен содержать не менее 8 символов!");
 	    }
 	    if(!preg_match("#[0-9]+#",$password)) {
-        	return ["status" => false, "message" => "Пароль должен содержать не менее 1 цифры!"];
+        	return array("status" => false, "message" => "Пароль должен содержать не менее 1 цифры!");
 	    }
 	    if(!preg_match("#[A-Z]+#",$password)) {
-        	return ["status" => false, "message" => "Пароль должен содержать не менее 1 заглавной буквы!"];
+        	return array("status" => false, "message" => "Пароль должен содержать не менее 1 заглавной буквы!");
 	    }
 	    if(!preg_match("#[a-z]+#",$password)) {
-        	return ["status" => false, "message" => "Пароль должен содержать не менее 1 строчной буквы!"];
+        	return array("status" => false, "message" => "Пароль должен содержать не менее 1 строчной буквы!");
 	    } 
 
-	    return ["status" => true, "message" => "Пароль указан верно!"];
+	    return array("status" => true, "message" => "Пароль указан верно!");
 
 
 	}
--- components/comp.home.php
+++ components/comp.home.php
@@ -1,9 +1,9 @@
 <?PHP
 require_once($_SERVER["DOCUMENT_ROOT"]."/assets/general/class.account.php");
-
-echo $_SESSION['user_id'];
-
 Account::init();
+
+echo Account::$ID;
+
 if (Account::$AUTH) {
 	echo "True";
 }else{
@@ -13,5 +13,12 @@ if (Account::$AUTH) {
 
 echo "<pre>";
 $user_info = ["email" => "dkds.adk@mail.ru", "password" => "dkD3kdmsds"];
-var_dump(Account::signup($user_info));
+// var_dump(Account::signup($user_info));
+
+var_dump(Account::auth("dkds.adk@mail.ru", "dkD3kdmsds"));
+// var_dump(Account::exit());
 echo "</pre>";
+
+if (Account::$AUTH) {
+	echo Account::$EMAIL;
+}

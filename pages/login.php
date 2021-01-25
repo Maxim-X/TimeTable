c3d7288e86bc62a697f7c6b5210d9aba2b44a891
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2021-01-25 16:44:39 +0500

    Дополнение к странице авторизации

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,10 @@
-<script src='https://www.google.com/recaptcha/api.js'></script>
 <section id="login">
 	<div class="container">
 		<div class="row">
-			<div class="offset-xxl-4 offset-md-2 col-xxl-4 col-md-8 col-sm-12 col-12">
+			<div class="col-xxl-4 col-xl-4 col-lg-3 d-md-none d-none d-lg-block d-xl-block d-xxl-block">
+				<img src="/resources/images/logo.svg" alt="Logo">
+			</div>
+			<div class="col-xxl-4 col-xl-4 col-lg-6 col-md-12 col-sm-12 col-12">
 				<div class="main_user_login">
 					<h3>С возвращением!</h3>
 					<p>Мы так рады видеть вас снова!</p>
@@ -16,20 +18,26 @@
 						    <label for="inputLoginAuth" class="form-label">Логин</label>
 							<input type="text" name="inputLogin" class="form-control" id="inputLoginAuth" placeholder="Введите ваш логин" value="<?=$login;?>" required>
 						</div>
-						<div class="mb-3">
+						<div class="mb-4">
 						    <label for="inputPasswordAuth" class="form-label">Пароль</label>
-							<input type="password" name="inputPassword" class="form-control" id="inputPasswordAuth" placeholder="Введите ваш пароль" value="<?=$password;?>" required>
+							<input type="password" name="inputPassword" class="form-control" id="inputPasswordAuth" placeholder="Введите ваш пароль" required>
 						</div>
 						<div class="mb-3">
-							<!-- добавление элемента div -->
+
 							<div class="g-recaptcha" data-sitekey="6LfauDoaAAAAAIunTLnBB5i4OvlC_GVVaBRzlW6X"></div>
 							
 						</div>
-						<button type="submit" name="authUser" class="btn btn-primary" style="width: 100%;">Войти</button>
+						<button type="submit" name="authUser" class="btn btn-primary btn-def" style="width: 100%;">Войти</button>
 					</form>
+					<div class="login_url">
+						<a href="#">Забыли пароль?</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
+
+
+<script src='https://www.google.com/recaptcha/api.js'></script>

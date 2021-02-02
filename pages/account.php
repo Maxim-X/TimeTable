ef3c01b4b6b327a2c565147ed002c2a1a2747fa8
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2021-02-02 18:53:20 +0500

    Добавил блоки поиска в account.php

--- pages/account.php
+++ pages/account.php
@@ -9,6 +9,106 @@
 			<section id="content">
 				<div class="main_content">
 					<h1 class="main_header">Панель администратора <span class="highlight">УКСИВТ</span></h1>
+
+					<div class="section_all_func">
+						<div class="left_func">
+							<div class="line_left_block">
+								<div class="block_func">
+									<div class="name">
+										<a href="#">
+											<span name="title">Обучающиеся</span>
+											<span name="count">712 чел.</span>
+											<div class="arrow"><img src="resources/images/icon/right-arrow-angle.svg"></div>
+										</a>
+									</div>
+									<div class="inp_search" id="student">
+										<label for="student_search">Быстрый поиск</label>
+										<div class="search">
+											<input type="text" id="student_search" name="student_search" class="form-control" placeholder="ФИО студента или группа">
+										</div>
+										<div class="content_search">
+											<div class="name_group_search">Обучающиеся</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+													<div class="optionally">19ИС-1</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+													<div class="optionally">19ИС-1</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+													<div class="optionally">19ИС-1</div>
+												</a>
+											</div>
+											<div class="name_group_search">Группы</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">18П-19</div>
+													<div class="optionally">24 Ученика</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">18П-19</div>
+													<div class="optionally">24 Ученика</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">18П-19</div>
+													<div class="optionally">24 Ученика</div>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="block_func">
+									<div class="name">
+										<a href="#">
+											<span name="title">Преподаватели</span>
+											<span name="count">71 чел.</span>
+											<div class="arrow"><img src="resources/images/icon/right-arrow-angle.svg"></div>
+										</a>
+									</div>
+									<div class="inp_search" id="teacher">
+										<label for="teacher_search">Быстрый поиск</label>
+										<div class="search">
+											<input type="text" id="teacher_search" name="teacher_search" class="form-control" placeholder="ФИО преподавателя">
+										</div>
+										<div class="content_search">
+											<!-- <div class="name_group_search">Преподаватели</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+												</a>
+											</div>
+											<div class="line_cont_search">
+												<a href="#">
+													<div class="main_name">Потапов Гавриил Степанович</div>
+												</a>
+											</div> -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 		</div>
